12ed0dfe922d07439edea19481162b09d77554fa
tiagofvx <tiagofvx@gmail.com>
2021-06-14 15:12:53 -0300

    fc

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ecd1ade37102f191057fb6b531d22dec'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93653fab-d61c-450d-af41-614ca2c77cea'
+};
