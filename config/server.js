3b4b1a70d8a531cfd13c8a0c0569ee809edc5d41
tiagofvx <tiagofvx@gmail.com>
2021-06-15 13:44:39 -0300

    fixes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://54.207.188.131/',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ecd1ade37102f191057fb6b531d22dec'),
